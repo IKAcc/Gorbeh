ef55901dc3093137f54a9564a597bbdc41cca582
Javid Izadfar <javid.izadfar@gmail.com>
2016-12-26 22:20:37 +0330

    started Treanformation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var postcss_import = require('postcss-import');
 var postcss_simple_vars = require('postcss-simple-vars');
 
@@ -7,6 +8,7 @@ gulp.task('css', function() {
   var plugins = [
       postcss_import,
       postcss_simple_vars,
+      autoprefixer
   ];
   return gulp.src('postcss/gorbeh-icons.css')
   .pipe(postcss(plugins))
@@ -18,4 +20,5 @@ gulp.task('default', ['css'], function() {
 
   // watch for CSS changes
   gulp.watch('./postcss/*.css', ["css"]);
+  gulp.watch('./postcss/partials/*.css', ["css"]);
 });
