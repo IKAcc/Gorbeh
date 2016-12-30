52fb818f0d19f1d1beae391b967db0ca061742fe
Javid Izadfar <javid.izadfar@gmail.com>
2016-12-30 20:35:22 +0330

    Add css minifire

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var postcss_import = require('postcss-import');
 var postcss_simple_vars = require('postcss-simple-vars');
+var cssNano = require('cssnano');
 
 gulp.task('css', function() {
   var plugins = [
@@ -15,8 +16,20 @@ gulp.task('css', function() {
   .pipe(gulp.dest('css'));
 });
 
+gulp.task('mini-css', function() {
+  var plugins = [
+      postcss_import,
+      postcss_simple_vars,
+      autoprefixer,
+      cssNano
+  ];
+  return gulp.src('postcss/gorbeh-icons.min.css')
+  .pipe(postcss(plugins))
+  .pipe(gulp.dest('css'));
+});
+
 // default gulp task
-gulp.task('default', ['css'], function() {
+gulp.task('default', ['css', 'mini-css'], function() {
 
   // watch for CSS changes
   gulp.watch('./postcss/*.css', ["css"]);
